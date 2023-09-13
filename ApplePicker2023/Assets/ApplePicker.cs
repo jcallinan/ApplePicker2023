282b8899ae4fc51dc649d2526b975187e6dbf6dc
Jeremy Callinan <jeremy.callinan@gmail.com>
2023-09-12 20:27:00 -0400

    Final version

--- ApplePicker2023/Assets/ApplePicker.cs
+++ ApplePicker2023/Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,19 +10,46 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
 
+    public void AppleMissed()
+    {
+        //destroy all of the falling apples
+        GameObject[] appleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tempGO in appleArray)
+        {
+            Destroy(tempGO);
+        }
+        //destroy one of the baskets
+        //getting the index of the last basket in the list
+        //basketList[0]
+        //basketList[1]
+        //basketList[2]
+        int basketIndex = basketList.Count - 1;
+        GameObject basketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(basketGO);
+
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("Scene_0");
+        }
+
+    }
     // Update is called once per frame
     void Update()
     {
